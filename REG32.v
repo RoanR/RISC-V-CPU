9675998051908ab51ab70de97832ec59c4cf27b9
Roan <roan.j.richmond@gmail.com>
2022-11-21 14:04:29 +0000

    Added Stall Control to 32Bit Register

--- REG32.v
+++ REG32.v
@@ -2,11 +2,26 @@
 
 module REG32(input wire CLK,
 			 input wire CE,
-			 input wire [31:0] in,
-			 output reg [31:0] out);
+			 input wire ready_in,
+			 input wire valid_in,
+			 input wire [31:0] data_in,
+			 output wire ready_out,
+			 output wire valid_out, 
+			 output reg [31:0] data_out);
 
-// Simple 32-bit register. On rising edge of clock the value of out is updated to the value of in
+reg full;
+assign full = 0;
 
 always @(posedge CLK)
-if (CE == 1) in <= out;       
+	if (!full) ready_out <= 1;
+	if (ready_in) ready_out <= 1;
+
+// Simple 32-bit register. On rising edge of clock the value of out is updated to the value of in
+always @(posedge CLK)
+	if ((CE == 1) && (valid_in)) begin
+		data_out <= data_in;
+		full <= 1; 
+		valid_out <= 1;
+		ready_out <= 0;
+	end    
 endmodule
