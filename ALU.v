894c390813f0783948d9f5547808d5fdb0019903
Roan/home/p32065rr/Documents/Verilog-VDU/test.cc <roan.j.richmond@gmail.com>
2022-12-05 12:01:49 +0000

    Changed Port definitions in ALU. Added Unit Test Bench for ALU.

--- ALU.v
+++ ALU.v
@@ -1,30 +1,36 @@
-`default_nettype none
 
-module ALU( input  signed wire [31:0] alu_in_A, // rs1 or PC		
-        	input  signed wire [31:0] alu_in_B, // rs2 or Imm	
-		    input  wire [2:0] func,	            //ALU function
-			input  wire control,	            //extra control wire	 			
-		    output signed reg  [31:0] alu_out); //output wire
+module ALU(alu_in_A, alu_in_B, func, control, alu_out, clk);
 
-unsigned wire [31:0] in_A_unsigned = alu_in_A;
-unsigned wire [31:0] in_B_unsigned = alu_in_B;
+//Port Discipline
+input signed  [31:0] alu_in_A; // rs1 or PC
+input signed  [31:0] alu_in_B; // rs2 or Imm
+input wire    [2:0]  func;	   // ALU function
+input wire           control;  // extra control wire	
+input wire           clk;      // ALU is clock driven
+output reg signed [31:0] alu_out;  // output wire
 
-always @ (*) Begin 
-	case (func)
-		0: Begin 
-			if (control) result = alu_in_A; //For LUI Instructions
+
+wire unsigned [31:0] in_A_unsigned;
+assign in_A_unsigned = alu_in_A;
+wire unsigned [31:0] in_B_unsigned;
+assign in_B_unsigned = alu_in_B;
+
+always @ (posedge clk) begin 
+	case(func)
+		3'b000: begin 
+			if (control) alu_out = alu_in_A; //For LUI Instructions
 			else alu_out = alu_in_A + alu_in_B; end //ADD or ADDI
-		1: alu_out = alu_in_A << alu_in_B; //Left Shift Logical
-		2: alu_out = alu_in_A < alu_in_B; //Set less than
-		3: alu_out = in_A_unsigned < in_B_unsigned; //Set less than unsigned
-		4: alu_out = alu_in_A ^ alu_in_B; //XOR 
-		5: begin 
+		3'b001: alu_out = alu_in_A << alu_in_B; //Left Shift Logical
+		3'b010: alu_out = alu_in_A < alu_in_B; //Set less than
+		3'b011: alu_out = in_A_unsigned < in_B_unsigned; //Set less than unsigned
+		3'b100: alu_out = alu_in_A ^ alu_in_B; //XOR 
+		3'b101: begin 
 			if (control) alu_out = alu_in_A >>> alu_in_B; //Right Shift arithmetic
 			else alu_out = alu_in_A >> alu_in_B; end //Right Shift Logical
-		6: alu_out = alu_in_A || alu_in_B; //OR
-		7: alu_out = alu_in_A && alu_in_B; //AND
-		default: result = alu_in_A;
+		3'b110: alu_out = alu_in_A | alu_in_B; //OR
+		3'b111: alu_out = alu_in_A & alu_in_B; //AND
+		default: alu_out = alu_in_A;
     endcase
 end
-
+endmodule
 
---
+++ ALU_tb.v
@@ -0,0 +1,105 @@
+module ALU_tb ();                  
+
+reg  [31:0] operand_A;                    
+reg  [31:0] operand_B;                    
+reg   [2:0] func;                         
+reg         control;
+reg         clk;     
+wire [31:0] result;                       
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+ALU alu_dut(operand_A, operand_B, func, control, result, clk);
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+integer file_handle;                     
+integer tst, k;                           
+
+reg [31:0] A_in [0:8];                    
+reg [31:0] B_in [0:8];
+
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+initial begin
+    control = 0; func = 0;
+    A_in[0] = 32'h00000001; B_in[0] = 32'h00000001; // Base Case
+    A_in[1] = 32'h00000001; B_in[1] = 32'h00000000; // Testing Single 0 input
+    A_in[2] = 32'h00000000; B_in[2] = 32'h00000000; // Testing Double 0 input
+    A_in[3] = 32'hFFFFFFFF; B_in[3] = 32'hFFFFFFFF; // Testing Maximum input
+    A_in[4] = 32'hAAAAAAAA; B_in[4] = 32'h55555555; // Testing Alternating input
+    A_in[5] = 32'h00000002; B_in[5] = 32'h00000001; // For Shifts
+    A_in[6] = 32'hFFFFFFFF; B_in[6] = 32'h00000001; // Add to Maximum/Overflow Shift
+    A_in[7] = 32'h0000FF00; B_in[7] = 32'h00FF0000;
+    A_in[8] = 32'h800000FF; B_in[8] = 32'h00FF0000;
+
+    file_handle = $fopen("ALU_out.txt");
+    $fdisplay(file_handle, "Outcome from ALU tests\n");
+
+    //LUI Instrustions
+    $fdisplay(file_handle, "Testing LUI function");
+    for (tst = 0; tst < 9; tst = tst + 1) begin
+      control = 1;
+      operand_A = A_in[tst];                
+      operand_B = B_in[tst];
+      #100                                
+      $fdisplay(file_handle, "Input 1 = %32b, Input 2 = %32b, result = %32b", operand_A, operand_B, result);   
+    end
+    $fdisplay(file_handle, "");
+    func = 3'b101;
+    $fdisplay(file_handle, "Testing SRA function");
+    for (tst = 0; tst < 9; tst = tst + 1) begin
+      control = 1;
+      operand_A = A_in[tst];                
+      operand_B = B_in[tst];
+      #100                                
+      $fdisplay(file_handle, "Input 1 = %32b, Input 2 = %32b, result = %32b", operand_A, operand_B, result);   
+    end
+      $fdisplay(file_handle, "");
+      control = 0; func = 0;
+
+  for (k = 0; k < 8; k = k + 1)
+    begin
+
+    case (func)
+      0: $fdisplay(file_handle, "Testing ADD function");
+      1: $fdisplay(file_handle, "Testing SLL function");
+      2: $fdisplay(file_handle, "Testing SLT function");
+      3: $fdisplay(file_handle, "Testing SLTU function");
+      4: $fdisplay(file_handle, "Testing XOR function");
+      5: $fdisplay(file_handle, "Testing SRL  function");
+      6: $fdisplay(file_handle, "Testing OR function");
+      7: $fdisplay(file_handle, "Testing AND function");
+      default: $fdisplay(file_handle, "Unknown function");
+    endcase
+
+    for (tst = 0; tst < 9; tst = tst + 1) begin
+      operand_A = A_in[tst];                
+      operand_B = B_in[tst];
+      #100                                
+      $fdisplay(file_handle, "Input 1 = %32b, Input 2 = %32b, result = %32b", operand_A, operand_B, result);   
+      end
+
+    $fdisplay(file_handle, "");       
+    func = func + 1; 
+    end
+
+  #100;                                   
+  $fclose(file_handle);                     
+  $finish;                                    
+end 
+
+initial begin
+  $dumpfile("ALU_results.vcd");
+  $dumpvars;
+ end
+
+initial begin
+  clk = 0; 
+  forever #20 clk = !clk; 
+  $stop;
+end
+/* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -*/
+
+
+endmodule
