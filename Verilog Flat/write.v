28bdb960d35c76e176f621672d72a6ac3a72fa01
Roan/home/p32065rr/Documents/Verilog-VDU/test.cc <roan.j.richmond@gmail.com>
2023-03-23 17:17:35 +0000

    Added Comments to increase readability of WB

--- Verilog Flat/write.v
+++ Verilog Flat/write.v
@@ -6,10 +6,10 @@ module write(IR, RD, A, PC, clk,
 
 //Port Discipline
 //Input Wires
-input wire [31:0] IR;
-input wire [31:0] RD;
-input wire [31:0] PC;
-input wire [31:0] A;
+input wire [31:0] IR; //Instrcution to be executed
+input wire [31:0] RD; //Data if instrcution was a Load
+input wire [31:0] PC; //PC for current Instruction
+input wire [31:0] A;  //Data from Execute, if not Load
 input wire        clk;
 
 //Stall Controls
@@ -19,21 +19,22 @@ output reg  v_out;
 output reg  r_out;
 
 //Output Registers
-output reg [31:0] data;
-output reg [4:0]  address;
+output reg [31:0] data;    //Data to written back to registers
+output reg [4:0]  address; //Which register to write the data back to
 
 //Driver for address
 always @ (posedge clk) begin
     if (r_out & v_in) begin
-        if      (IR[6:0] == `STORE) address <= 4'b0000; //Store
+        if      (IR[6:0] == `STORE)  address <= 4'b0000; //Store
         else if (IR[6:0] == `BRANCH) address <= 4'b0000; //Branch
-        else                            address <= IR[11:7];
+        else                         address <= IR[11:7];
     end
 end
 
-function [31:0] read(input [31:0] IR, RD); begin
-    if ((IR[14:12] == `LB)|(IR[14:12] == `LH)|(IR[14:12] == `LW)) read = $signed(RD);
-    else if ((IR[14:12] == `LBU)|(IR[14:12] == `LHU))             read = $unsigned(RD);
+//Extending Value for Load Operation
+function [31:0] read(input [31:0] IR, RD); begin   
+    if ((IR[14:12] == `LB)|(IR[14:12] == `LH)|(IR[14:12] == `LW)) read = $signed(RD);  //Signed Load
+    else if ((IR[14:12] == `LBU)|(IR[14:12] == `LHU))             read = $unsigned(RD);//Unsigned Load
     end 
 endfunction
 
@@ -41,14 +42,13 @@ endfunction
 always @ (posedge clk) begin
     if (r_out & v_in) begin
         case(IR[6:0])
-        `JAL: data <= PC + 4;        //JAL
+        `JAL:  data <= PC + 4;        //JAL
         `JALR: data <= PC + 4;        //JALR
         `LOAD: data <= read(IR, RD);  //Load
         default:    data <= A;
         endcase
     end
 end
-
 
 //Stall controls 
 initial begin 
@@ -58,8 +58,8 @@ end
 
 //Driver for control signals
 always @ (posedge clk) begin
-    if (stall) begin v_out <= 0; r_out <= 0; end
-    else begin v_out <= v_in;    r_out <= 1; end
+    if (stall) begin v_out <= 0; r_out <= 0; end //Synchronous Stall
+    else begin v_out <= v_in;    r_out <= 1; end //Normal Operation
 end
 
 endmodule
