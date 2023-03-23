e4980824dc8f9ef7af412da4c0bd4e0edc1fc1cd
Roan/home/p32065rr/Documents/Verilog-VDU/test.cc <roan.j.richmond@gmail.com>
2023-03-23 17:19:02 +0000

    Added Comments to increase readability of DC

--- Verilog Flat/read.v
+++ Verilog Flat/read.v
@@ -6,15 +6,11 @@ module read(IR, WB_data, WB_address, PC, clk,
 
 //Port Discipline 
 //Input Wires
-input wire [31:0] IR;
-input wire [31:0] WB_data;
-input wire [31:0] PC;
-input wire [4:0]  WB_address;
-input wire        clk;
-
-
-//Internal Wires
-wire [7:0] op;
+input wire [31:0] IR;         //Instrcution to be processed
+input wire [31:0] WB_data;    //Data from WB stage
+input wire [31:0] PC;         //PC of instruction being processed
+input wire [4:0]  WB_address; //Register to write data back to, if 0, then dont perform register write
+input wire        clk; 
 
 //The registers 
 reg  [31:0] registers [0:31];
@@ -29,34 +25,37 @@ output reg  r_out;
 
 //Output Signals
 output reg [31:0] IR_out;
-output reg [31:0] A_out;
-output reg [31:0] B_out;
-output reg [31:0] I_out;
+output reg [31:0] A_out; //Register 1
+output reg [31:0] B_out; //Register 2
+output reg [31:0] I_out; //Immediate with sign extension
 output reg [31:0] PC_out;
 
+//Perform Immediate concatenation and then sign Extension
 function [31:0] calculate_i (input [31:0] IR); 
 begin
     case (IR[6:0])
-    `LUI:    calculate_i = {IR[31:12], 12'h000};
-    `AUIPC:  calculate_i = {IR[31:12], 12'h000};
+    `LUI:    calculate_i = {IR[31:12], 12'h000}; //Load Unsigned Immedate, so dont sign extend
+    `AUIPC:  calculate_i = {IR[31:12], 12'h000}; //Add Unsigned Immediate to PC, so dont sign extend
     `JAL:    calculate_i = $signed({IR[31], IR[19:12], IR[20], IR[30:21], {1'b0}});
-    `JALR:   calculate_i = $signed(IR[31:20]); //JALR
-    `BRANCH: calculate_i = $signed({IR[31], IR[7], IR[30:25], IR[11:8], {1'b0}}); //BEQ,BNE,BLT,BGE
-    `LOAD:   calculate_i = $signed(IR[31:20]); //LB, LH, LW, LBU, LHU
-    `STORE:  calculate_i = $signed({IR[31:25], IR[11:7]}); //SH, SW, SB
+    `JALR:   calculate_i = $signed(IR[31:20]); 
+    `BRANCH: calculate_i = $signed({IR[31], IR[7], IR[30:25], IR[11:8], {1'b0}}); 
+    `LOAD:   calculate_i = $signed(IR[31:20]); 
+    `STORE:  calculate_i = $signed({IR[31:25], IR[11:7]});
     `IM_ALU: begin
         if ((IR[14:12] == `SLL)|(IR[14:12] == `SR)) calculate_i = $unsigned(IR[24:20]); //Shifts
-        else calculate_i = $signed(IR[31:20]); //SLTUI, XORI, ORI, ANDI, ADDI, SLTI,
+        else calculate_i = $signed(IR[31:20]); //All other Immediate ALU operations
         end
-    default:    calculate_i = 32'hXXXXXXXX; 
+    default:    calculate_i = 32'hXXXXXXXX; //Shows Errors in waveform easier
     endcase
 end
 endfunction
 
+//Perform Writeback
 always @ (posedge clk) begin
     registers[WB_address] = WB_data;
 end
 
+//Driver for A and B - Fetch Registers
 always @ (posedge clk) begin
     if (r_out & v_in) begin
         if (IR[19:15] == 0 | IR[6:0] == `LUI)   A_out <= 32'h00000000;
@@ -70,6 +69,7 @@ always @ (posedge clk) begin
     end
 end
 
+//Driver for other outputs
 always @ (posedge clk) begin
     if (r_out & v_in) I_out <= calculate_i(IR);
     if (r_out & v_in) PC_out <= PC;
